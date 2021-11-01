bdff0078bd4f358586f265f46c008fecb2123463
antonioarjpi <antonioarjpi@gmail.com>
2021-11-01 11:53:21 -0300

    Subindo projeto back e front

---
+++ backend/src/test/java/com/devsimple/dashvendas/DashvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsimple.dashvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
