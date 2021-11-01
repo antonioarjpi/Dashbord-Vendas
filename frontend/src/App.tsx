98756b05f06e6bce8cd29932c84cec7f0e0f77ae
antonioarjpi <antonioarjpi@gmail.com>
2021-11-01 15:20:55 -0300

    Charts estáticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import Barchart from "assets/components/BarChart";
 import DataTable from "assets/components/DataTable";
+import Donutchart from "assets/components/DonutChart";
 import Footer from "assets/components/footer";
 import NavBar from "assets/components/NavBar";
 
@@ -7,7 +9,22 @@ function App() {
     <div>
       <NavBar/>
       <div className="container">
-      <h1 className="text-primary">Olá, mundo</h1>
+      <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+      <div className="row px-3">
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+          <Barchart/>
+        </div>
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Taxa de vendas (%)</h5>
+          <Donutchart/>
+        </div>
+        <div className="px-3">
+         <h1 className="text-primary py-3">Todas as vendas</h1>
+        </div>
+      </div>
+
       <DataTable/>
       </div>
       <Footer/>
--- frontend/src/assets/components/DataTable/index.tsx
+++ frontend/src/assets/components/DataTable/index.tsx
@@ -13,54 +13,48 @@ function DataTable() {
             </thead>
             <tbody>
                 <tr>
-                    <td>22/04/2021</td>
+                    <td>01/11/2021</td>
                     <td>Barry Allen</td>
                     <td>34</td>
                     <td>25</td>
                     <td>15017.00</td>
                 </tr>
                 <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>01/11/2021</td>
+                    <td>Eduardo Costa</td>
+                    <td>39</td>
+                    <td>35</td>
+                    <td>1700.00</td>
                 </tr>
                 <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>01/11/2021</td>
+                    <td>Weberson Santos</td>
+                    <td>30</td>
+                    <td>22</td>
+                    <td>10017.00</td>
                 </tr>
                 <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>01/11/2021</td>
+                    <td>Rony Adauto</td>
+                    <td>41</td>
+                    <td>37</td>
+                    <td>19087.00</td>
                 </tr>
                 <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>01/11/2021</td>
+                    <td>Lucas Santos</td>
+                    <td>20</td>
+                    <td>19</td>
+                    <td>11417.00</td>
                 </tr>
                 <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>01/11/2021</td>
+                    <td>Gustavo Henrique</td>
+                    <td>33</td>
+                    <td>33</td>
+                    <td>13019.00</td>
                 </tr>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
-                </tr>
+                
                 
             </tbody>
             </table>
