2b62f7e5698392cb8504165ccb60c5f86f53a1be
antonioarjpi <antonioarjpi@gmail.com>
2021-11-05 22:51:05 -0300

    Pagination

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -6,19 +6,19 @@ export type Sale ={
     deals: number;
     amount: number;
     date: string;
-    seller: Seller;
+    selle: Seller;
 }
 
-export type SalePage ={
-    content ?: Sale[];
-    last : boolean;
-    totalElements : number;
-    totalPages :number;
-    size ?: number;
-    number : number;
-    first : boolean;
-    numberOfElements ?: number;
-    empty ?: boolean;
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size?:  number;
+    number:  number;
+    first: boolean;
+    numberOfElements?:  number;
+    empty?: boolean;
 }
 
 export type SaleSum = {
--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,1 +1,2 @@
 export const BASE_URL = 'https://simpledev-dev.herokuapp.com'
+//export const BASE_URL = 'https://localhost:8080'
